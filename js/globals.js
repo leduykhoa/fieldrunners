2f1d5f8efc641741d96ef2c3be642593d57b1457
Lê Duy Khoa <leduykhoa060690@gmail.com>
2023-09-26 16:54:49 +0700

    Update

--- js/globals.js
+++ js/globals.js
@@ -3,4 +3,7 @@ let port = window.location.port;
 if (port == 80 || port == 443) {
     baseUrl = `${window.location.protocol}//${window.location.hostname}`;
 }
+if (baseUrl.indexOf(`leduykhoa.github.io`) > 0) {
+    baseUrl = `https://leduykhoa.github.io/fieldrunners`
+}
 // console.log(baseUrl);
